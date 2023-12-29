6faf2e8f036f43543e5b911e90be1f987e1c0bf6
Hafizul Haque <hafizul.haque@reddotdigitalit.com>
2023-12-29 23:47:39 +0600

    scroll element color effect

--- src/components/ScrollElement/ScrollElement.tsx
+++ src/components/ScrollElement/ScrollElement.tsx
@@ -8,9 +8,14 @@ type PropTypes = {
   changePercentage: string
 }
 
+const isPositive = (str: string): boolean => {
+  let val = +str;
+  return val > 0;
+}
+
 const ScrollElement = (data: PropTypes) => {
   return (
-    <div className="flex gap-x-4 items-center px-4 border-r text-sm tracking-tight">
+    <div className="flex gap-x-3 items-center pl-3 pr-5 border-r text-sm tracking-tight">
       <div className="flex relative">
         <img 
           className='relative left-[25%] top-[3px]' 
@@ -27,8 +32,8 @@ const ScrollElement = (data: PropTypes) => {
       </div>
       <p>{data.currency1+data.currency2}</p>
       <p>{data.currentRate}</p>
-      <p>{data.rateChange}</p>
-      <p>{data.changePercentage}%</p>
+      <p className={isPositive(data.rateChange)?'text-teal-300':'text-red-300'}>{data.rateChange}</p>
+      <p className={isPositive(data.changePercentage)?'text-teal-300':'text-red-300'}>({data.changePercentage}%)</p>
     </div>
   )
 }
