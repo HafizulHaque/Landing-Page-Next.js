adf3e0adbbaa108f1244fe5632aad9979207522a
Hafizul Haque <hafizul.haque@reddotdigitalit.com>
2023-12-29 12:50:04 +0600

    statistics section completed

--- src/components/Statistics/Statistics.tsx
+++ src/components/Statistics/Statistics.tsx
@@ -1,19 +1,36 @@
+'use client'
+
 import Button from "../Button/Button"
 
 const Statistics = () => {
   return (
-    <section className="max-w-screen-lg mx-auto p-4">
-      <div className="flex align-items-center gap-4">
-        <div className="w-1/2">
-          <h2>Introducing FundedNext Stellar Challenge 🙌</h2>
-          <ul>
-            <li>No Time Limit</li>
-            <li>Balance Based Drawdown</li>
-            <li>15% Profit Share from Challenge Phase</li>
+    <section className="max-w-screen-lg mx-auto p-4 py-32">
+      <div className="flex items-center space-x-4">
+        <div className="w-4/7">
+          <h2 className="text-4xl font-semibold tracking-wide leading-tight py-4">Introducing FundedNext Stellar Challenge 🙌</h2>
+          <ul className="pb-6 list-none">
+            <li className="pl-5 pr-2 py-2 flex items-center">
+              <div 
+                className="w-3 h-3 bg-cover bg-no-repeat bg-center mr-2" 
+                style={{backgroundImage: `url('./asterk.svg')`}}></div>
+                No Time Limit
+            </li>
+            <li className="pl-5 pr-2 py-2 flex items-center">
+              <div 
+                className="w-3 h-3 bg-cover bg-no-repeat bg-center mr-2" 
+                style={{backgroundImage: `url('./asterk.svg')`}}></div>
+                Balance Based Drawdown
+            </li>
+            <li className="pl-5 pr-2 py-2 flex items-center">
+              <div 
+                className="w-3 h-3 bg-cover bg-no-repeat bg-center mr-2" 
+                style={{backgroundImage: `url('./asterk.svg')`}}></div>
+                15% Profit Share from Challenge Phase
+            </li>
           </ul>
-          <Button/>
+          <Button text="Join Our Discord" onClick={() => {}}/>
         </div>
-        <div className="w-1/2">
+        <div className="w-3/7">
           <div>
             <img src="./stats.svg" alt="statistics"/>
           </div>
